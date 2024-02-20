2c628ebf771909f2f2a80a19af6eabe88b7e23bb
patel salar <patel.salar@skillassure.com>
2024-02-20 18:35:14 +0530

    task senwell backgrounder color change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bg-color-change';
+
+ 
+  colors:string[]=['red','green','blue']
+  defaultColor:string='initial';
+  currentColorIndex:number=0;
+  bgColor:string=this.defaultColor;
+
+  changeColor(){
+    if(this.currentColorIndex>=this.colors.length){
+      this.bgColor=this.defaultColor;
+      this.currentColorIndex=0
+    }else {
+      this.bgColor=this.colors[this.currentColorIndex]
+      this.currentColorIndex++
+    }
+  }
 }
